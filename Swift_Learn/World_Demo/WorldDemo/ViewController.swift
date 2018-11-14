2a409b0ca77f8cfc4b6a179ba6d913183510ce8f
Denixist <DZ@DenixistdeMacBook-Pro.local>
2018-11-14 15:45:45 +0800

    First

---
+++ Swift_Learn/World_Demo/WorldDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WorldDemo
+//
+//  Created by Denixist on 2018/11/6.
+//  Copyright (c) 2018年 Denixist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello, World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)    }
+}
+
